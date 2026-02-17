0f6a6af6625993ab68bc9635281682a7eaccef4c
l36031233-alt <l36031233@gmail.com>
2026-02-17 19:04:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,55 +9,62 @@ const div = document.getElementById("symptomList");
 
 symptomSet.forEach(symptom => {
     div.innerHTML += `
-        <label>
-        <input type="checkbox" value="${symptom}">
-        ${symptom}
-        </label><br>
+    <label>
+    <input type="checkbox" value="${symptom}"> ${symptom}
+    </label>
     `;
 });
 
 function checkDisease() {
-    const selected = [];
-    document.querySelectorAll("input:checked")
-        .forEach(cb => selected.push(cb.value));
 
-    let resultHTML = "";
+const selected = [];
+document.querySelectorAll("input:checked")
+.forEach(cb => selected.push(cb.value));
 
-    diseases.forEach(disease => {
-        let match = disease.symptoms.filter(s =>
-            selected.includes(s)).length;
+let resultHTML = "";
 
-        if(match >= 1) {
-            resultHTML += `
-                <h3>${disease.name}</h3>
+diseases.forEach(disease => {
 
-                <button onclick="openDetails('${disease.name}','medicine')">
-                Medicine
-                </button>
+let match = disease.symptoms.filter(s =>
+selected.includes(s)).length;
 
-                <button onclick="openDetails('${disease.name}','cause')">
-                Causing Agent
-                </button>
+if(match >= 2){
+resultHTML += `
+<h3>${disease.name}</h3>
 
-                <button onclick="openDetails('${disease.name}','food')">
-                Food to Avoid
-                </button>
+<button class="main-btn"
+onclick="openDetails('${disease.name}','medicine')">
+Medicine
+</button>
 
-                <button onclick="openDetails('${disease.name}','home')">
-                Home Remedies
-                </button>
-                <hr>
-            `;
-        }
-    });
+<button class="main-btn"
+onclick="openDetails('${disease.name}','cause')">
+Causing Agent
+</button>
 
-    if(resultHTML === "")
-        resultHTML = "No matching disease found.";
+<button class="main-btn"
+onclick="openDetails('${disease.name}','food')">
+Food to Avoid
+</button>
 
-    document.getElementById("result").innerHTML = resultHTML;
+<button class="main-btn"
+onclick="openDetails('${disease.name}','home')">
+Home Remedies
+</button>
+
+<hr>
+`;
 }
 
-function openDetails(name, section) {
-    window.location.href =
-        `details.html?disease=${name}&section=${section}`;
+});
+
+if(resultHTML === "")
+resultHTML = "No matching disease found.";
+
+document.getElementById("result").innerHTML = resultHTML;
+}
+
+function openDetails(name, section){
+window.location.href =
+`details.html?disease=${encodeURIComponent(name)}&section=${section}`;
 }
